0ceb356a973350f8798ec909c831491cc6ce6e29
Dylan Hunt <dh@smartlaunch.com>
2016-08-03 21:37:08 +0800

    Major update for people to actually see -- friendlier, better, and ignoring some secrets goodies.

--- tol/server.js
+++ tol/server.js
@@ -1,38 +1,51 @@
+// ##############################################################################################
+// Created June, 2016
+// Created by dylan@imperium42.com (Imperium42 Game Studio)
+// Created for Throne of Lies: The Online Game of Lies and Deceit @ https://www.ThroneofLies.com
+// Like what you see? Toss us some love <3 by support our game ^
+// ##############################################################################################
+
 // Init
 console.log('Node Started..');
 var PORT = 55555;
 
-var fs = require('fs');
-var csv = require('fast-csv');
-var https = require('https');
-var express = require('express');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var cors = require('express-cors');
-var crypto = require('crypto');
-var CryptoJS = require('crypto-js');
-var Base64 = require('crypto-js/enc-base64');
-var PlayFabClient = require('./playfab-node/PlayFabClient.js');
-var gameSparks = new require('./gamesparks-node/GameSparks.js');
+var fs = require('fs');					 						 // Filestream
+var csv = require('fast-csv');			 						 // Read CSV files
+var https = require('https');			 						 // Create secure HTTPS server
+var express = require('express');		 					     // The main web handler
+var bodyParser = require('body-parser'); 						 // Allows for querying JSON formats
+var MongoClient = require('mongodb').MongoClient;				 // NoSQL DB
+var cors = require('express-cors');								 // Cross-Origin access rules
+var crypto = require('crypto');									 // Integrated cryptography library for hash/encrypt
+var Base64 = require('crypto-js/enc-base64');					 // Extension to Crypto for Base64 algorithms
+var PlayFabClient = require('./playfab-node/PlayFabClient.js');	 // PlayFab BaaS SDK
+var gameSparks = new require('./gamesparks-node/GameSparks.js'); // GameSparks BaaS SDK
 
 var app = express();
 
-// Use SSL
-https.createServer({
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
-}, app).listen(PORT);
-
-console.log('Listening on PORT ' + PORT);
+// Use SSL -- must have your own certificates at ./ssl/ (both key.pem and cert.pem). You may need to Google this.
+try {
+  https.createServer({
+    key: fs.readFileSync('./ssl/key.pem'),
+    cert: fs.readFileSync('./ssl/cert.pem')
+  }, app).listen(PORT);	
+  console.log('Listening on PORT ' + PORT);
+} catch (e) {
+	console.log("**WARNING: You forgot to copy your 'key.pem' and 'cert.pem' SSL/TLS files to /tol/data/");
+}
 
 // Allow JSON queries
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Read JSON keys file sync
-var secretKeys = JSON.parse(fs.readFileSync('./data/secret-keys.json', 'utf8'));
+// Read JSON keys file sync - You need to edit ./data/secret-keys-json with your own title+secret
+try {
+  var secretKeys = JSON.parse(fs.readFileSync('./data/secret-keys.json', 'utf8'));
+} catch (err) {
+  console.log("**WARNING: You forgot to make 'secret-keys.json' @ /tol/data/");
+}
 
-// Read CSV (for example, CD keys)
+// EXPERIMENTAL: Read CSV (for example, CD keys)
 //fs.createReadStream('data/dmt-keys.csv')
 //  .pipe(csv())
 //  .on('data', function(data) {
@@ -44,18 +57,32 @@ var secretKeys = JSON.parse(fs.readFileSync('./data/secret-keys.json', 'utf8'));
 //    console.log('Read finished');
 //  });
 
-// Allow CORS
+// Allow CORS : Put your own sites here
 app.use(cors({
   allowedOrigins: [
     '*.throneoflies.com', '*.imperium42.com'
   ]
 }));
 
+// Lazy JSON.Stringify() for pretty logs and debugging
+function J(obj, pretty) {
+    if (!pretty)
+        return JSON.stringify(obj);             // js obj >> json
+    else
+        return JSON.stringify(obj, null, 2);    // Better for logs+humans
+}
+
+// Simple date for logs
+function GetDateTime() {
+    return new Date().toISOString()
+        .replace(/T/, ' ')      // replace T with a space
+        .replace(/\..+/, '');   // delete the dot and everything after
+}
+
 // ############################################################################
 // PlayFab SDK
-// ############################################################################
-//
-// Init
+
+// Init - The title/secret will be obtained from the JSON file above
 var pfTitleId = secretKeys['pfTitleId'];
 var pfSecret = secretKeys['pfSecret'];
 PlayFabClient.settings.titleId = pfTitleId;
@@ -65,10 +92,36 @@ PlayFabClient.settings.developerSecretKey = pfSecret;
 function PFInitPost(req, routeName) {
   console.log( '\n' + GetDateTime() );
   console.log('####################################');
-  console.log('POST request to "/pf-loginwithfb" ..');
+  console.log('POST request to "' + routeName + '" ..');
   console.log( '<< PF (REQ): ' + J(req.body, true) + '\n' );
 }
 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Handles all functions from PlayFab that simply returns the "vanilla" value
+function PFGenericCallback(res, err, data) {
+    if (err) {
+        // Fail
+        console.log( 'PF (PF_ERR): ' + J(err, true) + ' >>' );
+        res.send(data);
+    } else if (data) {
+        // Success
+        console.log( 'PF (RESULT): ' + J(data, true) + ' >>' );
+        res.send(data);
+    } else {
+        // (?) Something went wrong -- null..?
+        console.log('PF (UNKNOWN_ERR): Null..? >>');
+        if (!err) err = {
+            "err": 'unknown'
+        };
+        res.send(err);
+    }
+
+    // Done
+    console.log('####################################');
+    res.end();
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 // https://api.playfab.com/Documentation/Client/method/LoginWithFacebook
 app.post('/pf-loginwithfb', (req, res) => {
   // Init
@@ -102,60 +155,72 @@ app.post('/pf-loginwithfb', (req, res) => {
   });
 });
 
-// ##############################################################################
-// GameSparks SDK
-// ##############################################################################
-var gsApiKey = secretKeys['gsApiKey'];
-var gsSecret = secretKeys['gsSecret'];
-var socketCount = 50;
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// https://api.playfab.com/Documentation/Client/method/UnlinkFacebookAccount
+app.post('/pf-unlinkfbacct', (req, res) => {
+    // Init
+    PFInitPost(req, '/pf-unlinkfbacct');
 
-gameSparks.initPreviewListener(gsApiKey, gsSecret, socketCount, onMessage, onInit);
-
-function onInit(res) {
-	console.log('GS (onInit): ' + res);
-}
-
-function onMessage(res) {
-	console.log( 'GS (onMessage): ' + JSON.stringify(res) );
-}
-
-// GET - Default
-app.get('/', (req, res) => {
-  console.log('GET request to "/"..');
-  res.header('Content-type', 'text/html');
-  return res.end('<h1>Welcome to the Throne of Lies API</h1>' +
-  '<h2>Check back later for public routes.</h2>');
+    // Send
+    PlayFabClient.LinkFacebookAccount(req.body, (err, data) => {
+        PFGenericCallback(res, err, data);
+    });
 });
 
-// POST -- GS Secret
-app.post('/gs-secret', (req, res) => {
-  console.log('POST request to "/gs-secret" with data:');
-  console.log(req.body);
-  var nonce = req.body.nonce;
+// ##############################################################################
+// EXPERIMENTAL: GameSparks SDK
 
-  if (nonce) {
-    var algorithm = 'sha256';
-    var hmac = crypto.createHmac(algorithm, nonce)
-      .update(gsSecret)
-      .digest('base64');
-   
-    var json = {
-    "gs": hmac
-    };
-    
-    console.log("GS: Sending " + J(json, true) + " >>");
-    res.send( json );
-    res.end();
-  
-  } else { 
-    var json = {
-      "gs": "ERROR_NO_NONCE"
-    };
-    
-    res.send(json);
-    res.end();
-  } 
-});
+//var gsApiKey = secretKeys['gsApiKey'];
+//var gsSecret = secretKeys['gsSecret'];
+//var socketCount = 50;
+//
+//gameSparks.initPreviewListener(gsApiKey, gsSecret, socketCount, onMessage, onInit);
+//
+//function onInit(res) {
+//	console.log('GS (onInit): ' + res);
+//}
+//
+//function onMessage(res) {
+//	console.log( 'GS (onMessage): ' + JSON.stringify(res) );
+//}
+//
+//// GET - Default @ https://api.ThroneOfLies.com:55555/
+//app.get('/', (req, res) => {
+//  console.log('GET request to "/"..');
+//  res.header('Content-type', 'text/html');
+//  return res.end('<h1>Welcome to the Throne of Lies API</h1>' +
+//  '<h2>Check back later for public routes.</h2>');
+//});
+//
+//// POST -- Encrypt GS secret >> Send it back
+//app.post('/gs-secret', (req, res) => {
+//  console.log('POST request to "/gs-secret" with data:');
+//  console.log(req.body);
+//  var nonce = req.body.nonce;
+//
+//  if (nonce) {
+//    var algorithm = 'sha256';
+//    var hmac = crypto.createHmac(algorithm, nonce)
+//      .update(gsSecret)
+//      .digest('base64');
+//
+//    var json = {
+//	  "gs": hmac
+//    };
+//
+//    console.log("GS: Sending " + J(json, true) + " >>");
+//    res.send( json );
+//    res.end();
+//
+//  } else {
+//    var json = {
+//      "gs": "ERROR_NO_NONCE"
+//    };
+//
+//    res.send(json);
+//    res.end();
+//  }
+//});
 
 // POST -- Keys testing
 //app.post('/keys', (req, res) => {
@@ -182,18 +247,3 @@ app.post('/gs-secret', (req, res) => {
 //    console.log('Listening on 3000')
 //  })
 //})
-
-// Lazy JSON.Stringify()
-function J(obj, pretty) {
-  if (!pretty)
-    return JSON.stringify(obj); // js obj >> json
-  else
-    return JSON.stringify(obj, null, 2); // Better for logs+humans
-}
-
-// Simple date
-function GetDateTime() {
-  return new Date().toISOString().
-    replace(/T/, ' ').      // replace T with a space
-    replace(/\..+/, '');    // delete the dot and everything after
-}
