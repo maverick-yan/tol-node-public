aca8421f8d147afd66a88108bd2eb1dc6b4da91c
Dylan Hunt <dylanh724@gmail.com>
2016-08-27 14:35:21 +0800

    Updated readme png, added * CORS to public GET '/', ignored node_modules

--- tol2/routes/index.js
+++ tol2/routes/index.js
@@ -1,5 +1,13 @@
+var cors = require('express-cors');
 var express = require('express');
 var router = express.Router();
+
+// Set cross-origin rules
+router.use(cors({
+  allowedOrigins: [
+    '*'
+  ]
+}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
