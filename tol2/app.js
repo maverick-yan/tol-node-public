34cbaa274fa492ae4d3012a289d53efb9787a253
Dylan Hunt <dh@smartlaunch.com>
2016-09-05 18:15:46 +0800

    +mailchimp use

--- tol2/app.js
+++ tol2/app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // ROUTES >>
 app.use('/', publicRoutes);
 app.use('/playfab', playfab);
-//app.use('/mailchimp', mailchimp);
+app.use('/mailchimp', mailchimp);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
